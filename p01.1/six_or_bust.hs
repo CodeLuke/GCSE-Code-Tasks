3787889380fee627e83bf1fed4a0fc6a0440c374
Luke Briggs <lukebriggs02@gmail.com>
2021-06-12 12:33:33 +0100

    CHange comments to work with doctest module

--- p01.1/six_or_bust.hs
+++ p01.1/six_or_bust.hs
@@ -1,42 +1,26 @@
-{-
-Problem:
 
-    
-A desperate gambler has bet all of his money on a final last throw of the die.
-    
-If he rolls a six, he survives to fight another day.
-    
-Anything else, and he gets thrown to the thugs waiting for him in the back alley.
+-- |Problem:
+-- |A desperate gambler has bet all of his money on a final last throw of the die.
+-- |If he rolls a six, he survives to fight another day.    
+-- |Anything else, and he gets thrown to the thugs waiting for him in the back alley.
+-- |The goneBust function takes an integer n, and should print either "Pheww!" or
+-- |"HELP! HELP!" depending on whether he survives or not.
+--
+-- Tests:
+-- >>> goneBust(2)
+-- "HELP! HELP!"
+--    
+-- >>> goneBust(6)
+-- "Pheww!"
+--    
+-- >>> goneBust(10)    
+-- "HELP! HELP!"
 
-    
-The goneBust function takes an integer n, and should print either "Pheww!" or
-    
-"HELP! HELP!" depending on whether he survives or not.
 
-
-Tests:
-
-    
->>> goneBust(2)
-"Help! Help!"
-    
-HELP! HELP!
-    
->>> goneBust(6)
-"Pheww!"
-    
-Pheww!
-    
->>> goneBust(10)
-"Help! Help!"
-    
-HELP! HELP!
-
--}
 module GoneBust where
 
 goneBust :: Integer -> String  
 goneBust roll = 
     if roll == 6
         then "Pheww!"
-        else "Help! Help!"
+        else "HELP! HELP!"
